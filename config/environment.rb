f05663f38136801f90e9cbed81492881fbd449e4
Brian Sullivan <brian@civicevolution.org>
2010-09-21 01:43:59 -0700

    differentiate apps based on installed directory which exposes application name, v1

--- config/environment.rb
+++ config/environment.rb
@@ -42,4 +42,4 @@ Rails::Initializer.run do |config|
   config.action_mailer.register_template_extension('haml')
 end
 
-APP_VERSION = RAILS_ROOT
+APP_VERSION = RAILS_ROOT.match(/\/data\/(\w+)\//)[1]
