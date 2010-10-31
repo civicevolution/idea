c36ed6299b0f2e6f746f6740e4a7c8b8db5c9b64
Brian Sullivan <brian@civicevolution.org>
2010-10-31 11:33:49 -0700

    create a universal sign in with access code system, refit welcome controller, add admin email team feature, admin admin rights - add new files

---
+++ app/models/admin_mailer.rb
@@ -0,0 +1,21 @@
+class AdminMailer < ActionMailer::Base
+
+  def email_message(recipient, plain_text_message, html_message, sent_at = Time.now)
+    subject    'Email message'
+    recipients "#{recipient.first_name} #{recipient.last_name} <#{recipient.email}>"
+    from       "\"CivicEvolution\" <support@civicevolution.org>"
+    sent_on    sent_at
+    
+    body      :plain_text_message => plain_text_message, :html_message => html_message
+  end
+
+  def reset_password(member, mcode, host, sent_at = Time.now)
+    subject    'Reset your CivicEvolution password'
+    recipients "#{member.first_name} #{member.last_name} <#{member.email}>"
+    from       "\"CivicEvolution\" <no-reply@auto.civicevolution.org>"
+    sent_on    sent_at
+    
+    body       :member => member, :mcode => mcode, :host => host
+  end
+
+end
