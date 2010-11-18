71925358e2d72d0c20a91f897f0168ccad6094c0
Brian Sullivan <brian@civicevolution.org>
2010-11-18 00:59:17 -0800

    split recipients between cgg and 2029

--- app/models/call_to_action_email.rb
+++ app/models/call_to_action_email.rb
@@ -52,7 +52,7 @@ class CallToActionEmail < ActiveRecord::Base
           %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, 0 AS team_id
           FROM members m, initiative_members im
           WHERE m.id = im.member_id
-          AND im.initiative_id IN (1,2)
+          AND im.initiative_id = 2
           AND m.id NOT IN (SELECT member_id FROM team_registrations)|)
 
         when 2
@@ -60,7 +60,7 @@ class CallToActionEmail < ActiveRecord::Base
           Member.find_by_sql(
           %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
           FROM members m, initiative_members im, team_registrations tr, teams t
-          WHERE im.initiative_id IN (1,2)
+          WHERE im.initiative_id = 2
           AND t.launched = false
           AND m.id = im.member_id
           AND tr.member_id = m.id
@@ -74,7 +74,7 @@ class CallToActionEmail < ActiveRecord::Base
           Member.find_by_sql(
           %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
           FROM members m, initiative_members im, team_registrations tr, teams t
-          WHERE im.initiative_id IN (1,2)
+          WHERE im.initiative_id = 2
           AND m.id NOT IN (SELECT member_id FROM comments WHERE member_id = m.id AND team_id = t.id)
           AND t.launched = true
           AND m.id = im.member_id
@@ -90,7 +90,7 @@ class CallToActionEmail < ActiveRecord::Base
           %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id,
           (SELECT COUNT(*) FROM comments WHERE member_id = m.id AND team_id = t.id) AS comments
           FROM members m, initiative_members im, team_registrations tr, teams t, comments c
-          WHERE im.initiative_id IN (1,2)
+          WHERE im.initiative_id = 2
           AND m.id = c.member_id
           AND t.id = c.team_id
           AND t.launched = true
@@ -99,6 +99,62 @@ class CallToActionEmail < ActiveRecord::Base
           AND tr.team_id = t.id
           AND t.id > 10018
           ORDER BY comments, title, m.id|)
+          
+        when 5
+          #Registered, no team (49)
+          Member.find_by_sql(
+          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, 0 AS team_id
+          FROM members m, initiative_members im
+          WHERE m.id = im.member_id
+          AND im.initiative_id = 1
+          AND m.id NOT IN (SELECT member_id FROM team_registrations)|)
+
+        when 6
+          #Joined a team that hasn't launched (31)
+          Member.find_by_sql(
+          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
+          FROM members m, initiative_members im, team_registrations tr, teams t
+          WHERE im.initiative_id = 1
+          AND t.launched = false
+          AND m.id = im.member_id
+          AND tr.member_id = m.id
+          AND tr.team_id = t.id
+          AND t.id > 10018
+          ORDER BY title, m.id|)
+          
+        when 7
+          #Joined a team that launched but haven't made a comment yet (27)
+
+          Member.find_by_sql(
+          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
+          FROM members m, initiative_members im, team_registrations tr, teams t
+          WHERE im.initiative_id = 1
+          AND m.id NOT IN (SELECT member_id FROM comments WHERE member_id = m.id AND team_id = t.id)
+          AND t.launched = true
+          AND m.id = im.member_id
+          AND tr.member_id = m.id
+          AND tr.team_id = t.id
+          AND t.id > 10018
+          ORDER BY title, m.id|)
+
+        when 8
+          #Joined a team and made at least one comment (21)
+
+          Member.find_by_sql(
+          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id,
+          (SELECT COUNT(*) FROM comments WHERE member_id = m.id AND team_id = t.id) AS comments
+          FROM members m, initiative_members im, team_registrations tr, teams t, comments c
+          WHERE im.initiative_id = 1
+          AND m.id = c.member_id
+          AND t.id = c.team_id
+          AND t.launched = true
+          AND m.id = im.member_id
+          AND tr.member_id = m.id
+          AND tr.team_id = t.id
+          AND t.id > 10018
+          ORDER BY comments, title, m.id|)
+          
+          
       end
     end
 end
