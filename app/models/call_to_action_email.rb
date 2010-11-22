07a8ec45239b7a32ad22e17407f5cab14ac14e96
Brian Sullivan <brian@civicevolution.org>
2010-11-22 12:23:43 -0800

    Select a team as recipients, do not send notificaitons if development and member_id > 10

--- app/models/call_to_action_email.rb
+++ app/models/call_to_action_email.rb
@@ -36,125 +36,135 @@ class CallToActionEmail < ActiveRecord::Base
     end
     
     def self.get_recipients_by_query( recipient_source )
-      logger.debug "recipient_source: #{recipient_source}, recipient_source.to_i: #{recipient_source.to_i}"
-      case recipient_source.to_i
-        when 0
-          #Registered, no team (49)
-          #Member.find_all_by_id([1,119]) #email('brian@civicevolution.org')]
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, id AS mem_id, 0 AS team_id
-          FROM members
-          WHERE id IN (1,119)|)
+      logger.debug "recipient_source: #{recipient_source}"
+      if recipient_source.match(/^team_id/)
+        team_id = recipient_source.match(/^team_id-(\d+)/)[1]
+        Member.find_by_sql(
+        [%q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
+        FROM members m, team_registrations tr, teams t
+        WHERE tr.team_id = ? 
+        AND tr.member_id = m.id
+        AND tr.team_id = t.id|,team_id])
+        
+      else
+        case recipient_source.to_i
+          when 0
+            #Registered, no team (49)
+            #Member.find_all_by_id([1,119]) #email('brian@civicevolution.org')]
+            Team.all(:select=> 'id, title, initiative_id', 
+              :conditions=>'initiative_id IN (1,2)', 
+              :order=>'initiative_id, title'
+            )
+          when 1
+            #Registered, no team (49)
+            Member.find_by_sql(
+            %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, 0 AS team_id
+            FROM members m, initiative_members im
+            WHERE m.id = im.member_id
+            AND im.initiative_id = 2
+            AND m.id NOT IN (SELECT member_id FROM team_registrations)|)
 
-        when 1
-          #Registered, no team (49)
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, 0 AS team_id
-          FROM members m, initiative_members im
-          WHERE m.id = im.member_id
-          AND im.initiative_id = 2
-          AND m.id NOT IN (SELECT member_id FROM team_registrations)|)
-
-        when 2
-          #Joined a team that hasn't launched (31)
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
-          FROM members m, initiative_members im, team_registrations tr, teams t
-          WHERE im.initiative_id = 2
-          AND t.launched = false
-          AND m.id = im.member_id
-          AND tr.member_id = m.id
-          AND tr.team_id = t.id
-          AND t.id > 10018
-          ORDER BY title, m.id|)
+          when 2
+            #Joined a team that hasn't launched (31)
+            Member.find_by_sql(
+            %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
+            FROM members m, initiative_members im, team_registrations tr, teams t
+            WHERE im.initiative_id = 2
+            AND t.launched = false
+            AND m.id = im.member_id
+            AND tr.member_id = m.id
+            AND tr.team_id = t.id
+            AND t.id > 10018
+            ORDER BY title, m.id|)
           
-        when 3
-          #Joined a team that launched but haven't made a comment yet (27)
+          when 3
+            #Joined a team that launched but haven't made a comment yet (27)
 
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
-          FROM members m, initiative_members im, team_registrations tr, teams t
-          WHERE im.initiative_id = 2
-          AND m.id NOT IN (SELECT member_id FROM comments WHERE member_id = m.id AND team_id = t.id)
-          AND t.launched = true
-          AND m.id = im.member_id
-          AND tr.member_id = m.id
-          AND tr.team_id = t.id
-          AND t.id > 10018
-          ORDER BY title, m.id|)
+            Member.find_by_sql(
+            %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
+            FROM members m, initiative_members im, team_registrations tr, teams t
+            WHERE im.initiative_id = 2
+            AND m.id NOT IN (SELECT member_id FROM comments WHERE member_id = m.id AND team_id = t.id)
+            AND t.launched = true
+            AND m.id = im.member_id
+            AND tr.member_id = m.id
+            AND tr.team_id = t.id
+            AND t.id > 10018
+            ORDER BY title, m.id|)
 
-        when 4
-          #Joined a team and made at least one comment (21)
+          when 4
+            #Joined a team and made at least one comment (21)
 
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id,
-          (SELECT COUNT(*) FROM comments WHERE member_id = m.id AND team_id = t.id) AS comments
-          FROM members m, initiative_members im, team_registrations tr, teams t, comments c
-          WHERE im.initiative_id = 2
-          AND m.id = c.member_id
-          AND t.id = c.team_id
-          AND t.launched = true
-          AND m.id = im.member_id
-          AND tr.member_id = m.id
-          AND tr.team_id = t.id
-          AND t.id > 10018
-          ORDER BY comments, title, m.id|)
+            Member.find_by_sql(
+            %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id,
+            (SELECT COUNT(*) FROM comments WHERE member_id = m.id AND team_id = t.id) AS comments
+            FROM members m, initiative_members im, team_registrations tr, teams t, comments c
+            WHERE im.initiative_id = 2
+            AND m.id = c.member_id
+            AND t.id = c.team_id
+            AND t.launched = true
+            AND m.id = im.member_id
+            AND tr.member_id = m.id
+            AND tr.team_id = t.id
+            AND t.id > 10018
+            ORDER BY comments, title, m.id|)
           
-        when 5
-          #Registered, no team (49)
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, 0 AS team_id
-          FROM members m, initiative_members im
-          WHERE m.id = im.member_id
-          AND im.initiative_id = 1
-          AND m.id NOT IN (SELECT member_id FROM team_registrations)|)
+          when 5
+            #Registered, no team (49)
+            Member.find_by_sql(
+            %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, 0 AS team_id
+            FROM members m, initiative_members im
+            WHERE m.id = im.member_id
+            AND im.initiative_id = 1
+            AND m.id NOT IN (SELECT member_id FROM team_registrations)|)
 
-        when 6
-          #Joined a team that hasn't launched (31)
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
-          FROM members m, initiative_members im, team_registrations tr, teams t
-          WHERE im.initiative_id = 1
-          AND t.launched = false
-          AND m.id = im.member_id
-          AND tr.member_id = m.id
-          AND tr.team_id = t.id
-          AND t.id > 10018
-          ORDER BY title, m.id|)
+          when 6
+            #Joined a team that hasn't launched (31)
+            Member.find_by_sql(
+            %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
+            FROM members m, initiative_members im, team_registrations tr, teams t
+            WHERE im.initiative_id = 1
+            AND t.launched = false
+            AND m.id = im.member_id
+            AND tr.member_id = m.id
+            AND tr.team_id = t.id
+            AND t.id > 10018
+            ORDER BY title, m.id|)
           
-        when 7
-          #Joined a team that launched but haven't made a comment yet (27)
+          when 7
+            #Joined a team that launched but haven't made a comment yet (27)
 
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
-          FROM members m, initiative_members im, team_registrations tr, teams t
-          WHERE im.initiative_id = 1
-          AND m.id NOT IN (SELECT member_id FROM comments WHERE member_id = m.id AND team_id = t.id)
-          AND t.launched = true
-          AND m.id = im.member_id
-          AND tr.member_id = m.id
-          AND tr.team_id = t.id
-          AND t.id > 10018
-          ORDER BY title, m.id|)
+            Member.find_by_sql(
+            %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id
+            FROM members m, initiative_members im, team_registrations tr, teams t
+            WHERE im.initiative_id = 1
+            AND m.id NOT IN (SELECT member_id FROM comments WHERE member_id = m.id AND team_id = t.id)
+            AND t.launched = true
+            AND m.id = im.member_id
+            AND tr.member_id = m.id
+            AND tr.team_id = t.id
+            AND t.id > 10018
+            ORDER BY title, m.id|)
 
-        when 8
-          #Joined a team and made at least one comment (21)
+          when 8
+            #Joined a team and made at least one comment (21)
 
-          Member.find_by_sql(
-          %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id,
-          (SELECT COUNT(*) FROM comments WHERE member_id = m.id AND team_id = t.id) AS comments
-          FROM members m, initiative_members im, team_registrations tr, teams t, comments c
-          WHERE im.initiative_id = 1
-          AND m.id = c.member_id
-          AND t.id = c.team_id
-          AND t.launched = true
-          AND m.id = im.member_id
-          AND tr.member_id = m.id
-          AND tr.team_id = t.id
-          AND t.id > 10018
-          ORDER BY comments, title, m.id|)
+            Member.find_by_sql(
+            %q|SELECT distinct first_name, last_name, email, m.id AS mem_id, title, t.id AS team_id,
+            (SELECT COUNT(*) FROM comments WHERE member_id = m.id AND team_id = t.id) AS comments
+            FROM members m, initiative_members im, team_registrations tr, teams t, comments c
+            WHERE im.initiative_id = 1
+            AND m.id = c.member_id
+            AND t.id = c.team_id
+            AND t.launched = true
+            AND m.id = im.member_id
+            AND tr.member_id = m.id
+            AND tr.team_id = t.id
+            AND t.id > 10018
+            ORDER BY comments, title, m.id|)
           
           
-      end
-    end
+        end # end case recipient_source
+      end # end if team id
+    end # end def get_recipients_by_query
 end
