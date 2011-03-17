29de7ff184ec2172b3bf3cd366e57d9d530e4624
Brian Sullivan <brian@civicevolution.org>
2011-03-17 10:43:05 -0700

    deploy/after_restart to restart notify, delayed, and mongrels for app_2029; included monitrc files into source control

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,10 @@
-#sudo "monit restart notify_d_app_2029"
-#sudo "monit restart delayed_job_2029"
-sudo "monit restart mongrel -g app_2029"
+begin
+  app = Rails.root.to_s.match(/\/data\/(\w+)/)[1]
+  if app == 'app_2029'
+    sudo "monit restart notify_d_#{app}"
+    sudo "monit restart delayed_job_#{app}"
+    sudo "monit restart mongrel -g #{app}"
+  end
+rescue
+  
+end
