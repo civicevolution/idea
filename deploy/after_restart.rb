f111f6dea3934fdc8a8ac2a4f9a4e1fbfe927bd1
Brian Sullivan <brian@civicevolution.org>
2011-03-17 11:48:49 -0700

    updated deploy/after_restart.rb to restart notify, delayed_job and mongrels for app_2029 deploys

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,10 +1,14 @@
+run "echo 'deploy/after_restart.rb release_path: #{release_path}' >> #{shared_path}/logs.log"
+
+#sudo "monit restart mongrel -g app_2029"
+
 begin
-  app = Rails.root.to_s.match(/\/data\/(\w+)/)[1]
+  app = release_path.root.to_s.match(/\/data\/(\w+)/)[1]
   if app == 'app_2029'
     sudo "monit restart notify_d_#{app}"
     sudo "monit restart delayed_job_#{app}"
     sudo "monit restart mongrel -g #{app}"
   end
-rescue
-  
+rescue Exception => e
+  run "echo 'deploy/after_restart.rb error: #{e.message}"
 end
