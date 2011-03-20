7592d795f5d9ff8efd0ffe95cb3f412c5a05c226
Brian Sullivan <brian@civicevolution.org>
2011-03-20 11:14:23 -0700

    disable deploy/after_restart as I try to move to unicorn

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,10 +1,10 @@
-begin
-  app = release_path.to_s.match(/\/data\/(\w+)/)[1]
-  if app == 'app_2029'
-    sudo "monit restart notify_d_#{app}"
-    sudo "monit restart delayed_job_#{app}"
-    sudo "monit restart mongrel -g #{app}"
-  end
-rescue Exception => e
-  run "echo 'deploy/after_restart.rb error: #{e.message}"
-end
+#begin
+#  app = release_path.to_s.match(/\/data\/(\w+)/)[1]
+#  if app == 'app_2029'
+#    sudo "monit restart notify_d_#{app}"
+#    sudo "monit restart delayed_job_#{app}"
+#    sudo "monit restart mongrel -g #{app}"
+#  end
+#rescue Exception => e
+#  run "echo 'deploy/after_restart.rb error: #{e.message}"
+#end
