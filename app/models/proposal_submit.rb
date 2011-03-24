e59d0c4c8c35ab38fb2b591fd9bf76a72c3c49e1
Brian Sullivan <brian@civicevolution.org>
2011-03-23 20:52:44 -0700

    allow anyone to submit the proposal for review

--- app/models/proposal_submit.rb
+++ app/models/proposal_submit.rb
@@ -7,12 +7,14 @@ class ProposalSubmit < ActiveRecord::Base
   def check_init_access
     # this is access check for the idea page version
     #allowed,message = InitiativeRestriction.allow_action({:team_id=>self.team_id}, 'submit_proposal', self.member)
-    @team = Team.find(self.team_id)
-    allowed = @team.org_id == self.member.id ? true : false
-    if !allowed
-      errors.add_to_base("Sorry, only the idea originator can submit this proposal.") 
-      return false
-    end
+    #@team = Team.find(self.team_id)
+    #allowed = @team.org_id == self.member.id ? true : false
+    #if !allowed
+    #  errors.add_to_base("Sorry, only the idea originator can submit this proposal.") 
+    #  return false
+    #end
+    
+    # allow anyone to submit for the immediate time being
   end
 
 end
