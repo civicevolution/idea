262775036ef4918bf2f18e9aa9fa67a19fa5560f
Brian Sullivan <brian@civicevolution.org>
2011-03-31 14:14:45 -0700

    Add logging for email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ G3::Application.configure do
   #config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :ses
+  config.action_mailer.logger = ActiveSupport::BufferedLogger.new( "#{Rails.root}/log/mail.sent.log" )
   
   #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.smtp_settings = {
