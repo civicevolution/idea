6b28572b3153ab10f8df006a9f92870e2f8a2baf
Brian Sullivan <brian@civicevolution.org>
2011-08-18 15:58:22 -0700

    Place suggest new idea for proposal in the notebook layout

--- app/controllers/plan_controller.rb
+++ app/controllers/plan_controller.rb
@@ -74,7 +74,7 @@ class PlanController < ApplicationController
     logger.debug "show form for suggest_new_idea"
     @proposal_idea = ProposalIdea.new params[:proposal_idea] unless @proposal_idea
     respond_to do |format|
-      format.html { render :template => "plan/suggest_new_idea", :layout=> 'welcome', :locals=>{:proposal_idea=>@proposal_idea} }
+      format.html { render :template => "plan/suggest_new_idea", :layout=> 'civicevolution', :locals=>{:proposal_idea=>@proposal_idea} }
       format.js
     end
     
