78c0ad68b234fdd99014de26db229d532f71ea5d
Brian Sullivan <brian@civicevolution.org>
2011-08-27 01:03:56 -0700

    enable character counter combined with autogrow

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -127,3 +127,35 @@ function getUrlVars()
     return vars;
 }
 var params = getUrlVars();
+
+function activate_text_counters_grow(els, height){
+	height = height? height : 50
+	els.each(
+		function(){
+			var el = $(this);
+			var span = el.next().find('span.char_ctr');
+			while(span.size() == 0 && el){
+				el = el.parent();
+				span = el.find('span.char_ctr');
+			}
+			if(span.size() == 1){
+				el = $(this);
+				var cnt = Number(span.html().match(/\d+/));
+				el.show_char_limit(cnt, {
+			    error_element: span,
+					status_element: span
+			  });
+				if(el[0].nodeName == 'TEXTAREA'){
+					// if the el is not displayed, IE gives it a width of 0 and the el cannot be setup to grow
+					if(el[0].offsetWidth > 0){
+						el.autoGrow({
+							minHeight  : height,
+							maxHeight : 500
+						});
+					}
+				}
+			}
+		}
+	)
+}
+
