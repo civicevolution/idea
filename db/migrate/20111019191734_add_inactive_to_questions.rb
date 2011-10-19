b213bcd3a0d88585169dfd242d8ab128451a6fd2
Brian Sullivan <brian@civicevolution.org>
2011-10-19 12:20:50 -0700

    Redo the migration of adding inactive to questions

--- db/migrate/20111019191734_add_inactive_to_questions.rb
+++ db/migrate/20111019191734_add_inactive_to_questions.rb
@@ -1,6 +1,8 @@
 class AddInactiveToQuestions < ActiveRecord::Migration
   def self.up
     add_column :questions, :inactive, :boolean, :default => false
+    
+    Question.update_all ["inactive = ?", false]
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111019161539) do
+ActiveRecord::Schema.define(:version => 20111019191734) do
 
   create_table "activities", :force => true do |t|
     t.integer  "member_id"
