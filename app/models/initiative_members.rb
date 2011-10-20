6bb3b3f0430756cdf72e78a04a44667b3be3ef37
Brian Sullivan <brian@civicevolution.org>
2011-10-19 21:55:11 -0700

    do not force participant description, this is only for 2029

--- app/models/initiative_members.rb
+++ app/models/initiative_members.rb
@@ -5,7 +5,7 @@ class InitiativeMembers < ActiveRecord::Base
 
   validates_presence_of :initiative_id
   validates_presence_of :member_id
-  validates_presence_of :member_category, :message => "- You must select one of the member descriptions"
+  #validates_presence_of :member_category, :message => "- You must select one of the member descriptions"
   validates_presence_of :accept_tos, :message => "- You must accept the Terms of Service"
 
   def o_type
