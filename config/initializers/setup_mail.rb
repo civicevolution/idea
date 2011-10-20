b428ccc4da4994756db87932c674625cd913595a
Brian Sullivan <brian@civicevolution.org>
2011-10-19 21:32:59 -0700

    do not intercept production email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,2 +1,3 @@
 require 'development_mail_interceptor'
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) unless Rails.env.production? && !Rails.root.to_s.match(/app_2029/).nil?
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) unless Rails.env.production?
+#ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) unless Rails.env.production? && !Rails.root.to_s.match(/app_2029/).nil?
