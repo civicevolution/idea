419af1991083a76829b6071783db3f308f4105f2
Brian Sullivan <brian@civicevolution.org>
2011-11-16 15:20:24 -0800

    Allow TP edits for the short term till I tie it to level

--- app/models/talking_point.rb
+++ app/models/talking_point.rb
@@ -41,6 +41,14 @@ class TalkingPoint < ActiveRecord::Base
     # either in team_registrations or a current or previous author of this TP
     cur_tp_member_id = self.member_id
     self.member_id = self.member.id
+
+    if self.member.confirmed
+      return true
+    else
+      errors.add(:base, "Sorry, you must be a conifrmed member to edit this talking point.") 
+      return false
+    end
+
     
     return true if cur_tp_member_id == 10 # if the initial version was entered by the admin account 10, let someone else edit it
 
