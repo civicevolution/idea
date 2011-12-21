2abc95ca011f521b009a3b07e70d56094eed9ee1
Brian Sullivan <brian@civicevolution.org>
2011-12-21 00:10:48 -0800

    add_call_to_action as a helper, add call to action config file and template, replace calls to action with call to helper in endorsements, curated_talking_points, question summary, submit, print

--- app/helpers/call_to_action_helper.rb
+++ app/helpers/call_to_action_helper.rb
@@ -1,2 +1,36 @@
 module CallToActionHelper
+  
+  def add_call_to_action(data = {} )
+    
+		conf = YAML.load_file("#{Rails.root}/config/calls_to_action.yaml")
+
+    primary_haml = ''
+    secondary_haml = ''
+    
+    debug = false
+    #debug = true if data[:id] == 'end_summary_print'
+    
+    # find a set that applies, or use the set if only one
+    conf['init_1'][data[:id]].each_value do |option|
+      debugger if debug
+      # iterate through the options to find the one(s) that match
+      if option['test'].nil? || eval(option['test'])
+        option['haml_strings'].each_pair do |key,haml_string|
+          if haml_string['test'].nil? || eval(haml_string['test'])
+            if key.match(/primary/) 
+              primary_haml += haml_string['haml'] 
+            else
+              secondary_haml += haml_string['haml'] 
+            end
+          end #haml_string test
+        end #haml_strings
+      end # if option['test']
+    end #options
+    debugger if debug
+    if primary_haml != '' || secondary_haml != ''
+      render :partial => 'shared/cta', :locals => { :primary => primary_haml, :secondary => secondary_haml, :custom_locals=>data }
+    end
+    
+  end
+  
 end
