80bfd96b4788f1dce6570a896544d0ea9a20aeab
Brian Sullivan <brian@civicevolution.org>
2012-04-11 22:12:44 -0700

    restore restart

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,8 +1,8 @@
 begin
   app = release_path.to_s.match(/\/data\/(\w+)/)[1]
   if app == 'app_2029' || app == 'tp'
-    #sudo "monit restart notify_d_#{app}"
-    #sudo "monit restart delayed_job_#{app}"
+    sudo "monit restart notify_d_#{app}"
+    sudo "monit restart delayed_job_#{app}"
     #sudo "monit restart mongrel -g #{app}"
   end
 rescue Exception => e
