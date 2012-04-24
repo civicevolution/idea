b4064fecca99c09cb1b4963a1f96aa6e05bf3fd0
Brian Sullivan <brian@civicevolution.org>
2012-04-24 11:25:37 -0700

    diable monit restart to see if it is more responsive

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,9 +1,9 @@
 begin
   app = release_path.to_s.match(/\/data\/(\w+)/)[1]
   if app == 'app_2029' || app == 'tp'
-    sudo "monit restart notify_d_#{app}"
-    sudo "monit restart delayed_job_#{app}"
-    #sudo "monit restart mongrel -g #{app}"
+    # sudo "monit restart notify_d_#{app}"
+    # sudo "monit restart delayed_job_#{app}"
+    # #sudo "monit restart mongrel -g #{app}"
   end
 rescue Exception => e
   run "echo 'deploy/after_restart.rb error: #{e.message}"
