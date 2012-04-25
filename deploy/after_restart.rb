3997ae1181680caeac840beb0d7f5f171022ad97
Brian Sullivan <brian@civicevolution.org>
2012-04-25 11:24:09 -0700

    new try to delay monit restart: sudo 'echo "sleep 180 && sudo monit restart notify_d_tp" | at now'

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,9 +2,9 @@ begin
   app = release_path.to_s.match(/\/data\/(\w+)/)[1]
   if app == 'app_2029' || app == 'tp'
      #sudo "sleep 180; monit restart notify_d_#{app}"
-     sudo 'echo "sleep 120 && monit restart notify_d_tp" | at now'
+     sudo 'echo "sleep 180 && sudo monit restart notify_d_tp" | at now'
      #sudo "sleep 300; monit restart delayed_job_#{app}"
-     sudo 'echo "sleep 240 && monit restart delayed_job_tp" | at now'
+     sudo 'echo "sleep 360 && sudo monit restart delayed_job_tp" | at now'
   end
 rescue Exception => e
   run "echo 'deploy/after_restart.rb error: #{e.message}"
