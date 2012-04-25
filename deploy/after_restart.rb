461a45ec9299d7834a7b8f8616a76df2686120bd
Brian Sullivan <brian@civicevolution.org>
2012-04-25 12:51:40 -0700

    Stagger and delay notification and delayed_job restart in deploy/after_restart, fix quotes in call to action yml file

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,9 +1,8 @@
 begin
   app = release_path.to_s.match(/\/data\/(\w+)/)[1]
-  if app == 'app_2029' || app == 'tp'
-    # sudo "monit restart notify_d_#{app}"
-    # sudo "monit restart delayed_job_#{app}"
-    # #sudo "monit restart mongrel -g #{app}"
+  if app == 'tp'
+     sudo 'echo "sleep 180 && sudo monit restart notify_d_tp" | at now'
+     sudo 'echo "sleep 360 && sudo monit restart delayed_job_tp" | at now'
   end
 rescue Exception => e
   run "echo 'deploy/after_restart.rb error: #{e.message}"
