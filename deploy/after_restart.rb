11ac5121418d803e188379ec7f5f7738414c7e04
Brian Sullivan <brian@civicevolution.org>
2012-04-25 09:04:27 -0700

    use sleep in deploy/after_restart to delay restarting notify and delayed_job

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,8 +1,8 @@
 begin
   app = release_path.to_s.match(/\/data\/(\w+)/)[1]
   if app == 'app_2029' || app == 'tp'
-     sudo "monit restart notify_d_#{app}"
-     sudo "monit restart delayed_job_#{app}"
+     sudo "sleep 180; monit restart notify_d_#{app}"
+     sudo "sleep 300; monit restart delayed_job_#{app}"
   end
 rescue Exception => e
   run "echo 'deploy/after_restart.rb error: #{e.message}"
