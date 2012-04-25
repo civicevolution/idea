e838465dd8b7dff4c0f11d729c519c8e28e58abb
Brian Sullivan <brian@civicevolution.org>
2012-04-25 09:30:01 -0700

    try a different deploy delay

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,9 +1,19 @@
 begin
   app = release_path.to_s.match(/\/data\/(\w+)/)[1]
   if app == 'app_2029' || app == 'tp'
-     sudo "sleep 180; monit restart notify_d_#{app}"
-     sudo "sleep 300; monit restart delayed_job_#{app}"
+     #sudo "sleep 180; monit restart notify_d_#{app}"
+     sudo 'echo "sleep 120 && monit restart notify_d_#{app}" | at now'
+     #sudo "sleep 300; monit restart delayed_job_#{app}"
+     sudo 'echo "sleep 240 && monit restart delayed_job_#{app}" | at now'
   end
 rescue Exception => e
   run "echo 'deploy/after_restart.rb error: #{e.message}"
 end
+
+
+# deploy/before_restart.rb
+on_app_servers_and_utilities do
+  node[:applications].each do |app_name, data|
+    sudo 'echo "sleep 20 && monit -g \#{app_name}_resque restart all" | at now'
+  end
+end		
