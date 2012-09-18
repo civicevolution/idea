41ce56d714481e88a00765f8f7afade252fdb741
Brian Sullivan <brian@civicevolution.org>
2012-09-18 08:01:14 -0700

    Reduce the assets I precompile to see if I can get this to work at all

--- config/application.rb
+++ config/application.rb
@@ -66,9 +66,15 @@ module G3
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.precompile += %w( application_ce.css application_ce_live.css application_ce.js application_ce_no_js.js application_ce_home.css opt/pure.js )
-    config.assets.precompile += %w( application_ce_home.css application_ce_home_all.js application_ce_home_home.js application_ce_home_no_js.js )
-    config.assets.precompile += %w( application_civic_home.css application_civic_home.js)
+    #config.assets.precompile += %w( application_ce.css application_ce_live.css application_ce.js application_ce_no_js.js application_ce_home.css opt/pure.js )
+    #config.assets.precompile += %w( application_ce_home.css application_ce_home_all.js application_ce_home_home.js application_ce_home_no_js.js )
+    #config.assets.precompile += %w( application_civic_home.css application_civic_home.js)
+    
+    config.assets.precompile += %w( application_ce.css application_ce.js application_ce_no_js.js )
+    
+    #config.assets.precompile += %w(  application_ce_live.css  application_ce_no_js.js application_ce_home.css opt/pure.js )
+    #config.assets.precompile += %w( application_ce_home.css application_ce_home_all.js application_ce_home_home.js application_ce_home_no_js.js )
+    #config.assets.precompile += %w( application_civic_home.css application_civic_home.js)
     
     
     #APP_NAME = Rails.root.to_s.match(/^\/data\//) ? Rails.root.to_s.match(/\/data\/(\w+)\//)[1] : Rails.root.to_s.match(/\/ce_development\/Rails\/(\w+)/)[1]
