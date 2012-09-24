28b58ddab59dd66e3b0a50ec861ae36add29cb93
Brian Sullivan <brian@civicevolution.org>
2012-09-24 08:38:54 -0700

    Fix after_restart to reference delayed_job_idea and notify_d_idea

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,8 +1,8 @@
 begin
   app = release_path.to_s.match(/\/data\/(\w+)/)[1]
-  if app == 'tp'
-     sudo 'echo "sleep 180 && sudo monit restart notify_d_tp" | at now'
-     sudo 'echo "sleep 360 && sudo monit restart delayed_job_tp" | at now'
+  if app == 'idea' || app == 'tp'
+     sudo 'echo "sleep 180 && sudo monit restart notify_d_#{app}" | at now'
+     sudo 'echo "sleep 360 && sudo monit restart delayed_job_#{app}" | at now'
   end
 rescue Exception => e
   run "echo 'deploy/after_restart.rb error: #{e.message}"
